88cdaf0a72a505ac70cd6f24032a0ac5d6f8970e
Tal Glanzman <talglanzman@gmail.com>
2025-01-07 16:33:54 +0200

    initial

---
+++ nile/app/app.cpp
@@ -0,0 +1,7 @@
+#include "nile/core/core.h"
+#include <iostream>
+
+int main(int _argc, char** _argv) {
+  core_function();
+}
+
---
+++ nile/core/core.cpp
@@ -0,0 +1,7 @@
+#include "core.h"
+#include <iostream>
+
+void core_function() {
+    std::cout << "Core function is working!" << std::endl;
+}
+
